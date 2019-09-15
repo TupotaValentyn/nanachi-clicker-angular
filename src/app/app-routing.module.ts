45d1ac0c3f5ba2ed851bd9b6cef0f5b0d235bd49
Volodymyr Polishchuk <volodymyrpo@tahometer.com>
2019-09-15 07:13:09 +0300

    route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {SetupComponentComponent} from "./components/setup-component/setup-component.component";
+import {GamePanelComponent} from "./components/game-panel/game-panel.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component: SetupComponentComponent,
+    path: '',
+  },
+  {
+    component: GamePanelComponent,
+    path: 'game',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
