831969fb138e2d7eaacf98cbe23db48b6ee4b114
TupotaValentyn <gm.tupota.valik@gmail.com>
2019-09-15 07:41:27 +0300

    Finish component

--- src/app/components/game-panel/game-panel.component.ts
+++ src/app/components/game-panel/game-panel.component.ts
@@ -16,6 +16,27 @@ export class GamePanelComponent implements OnInit {
       color: 'red',
       picture: '',
       score: 0
+    } ,{
+      width: 56,
+      height: 16,
+      name: 'NIP',
+      color: 'red',
+      picture: '',
+      score: 0
+    }, {
+      width: 46,
+      height: 26,
+      name: 'NANACHI',
+      color: 'red',
+      picture: '',
+      score: 0
+    }, {
+      width: 46,
+      height: 17,
+      name: 'CUSTOM',
+      color: 'red',
+      picture: '',
+      score: 0
     }
   ];
 
